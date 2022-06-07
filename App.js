a8dda0e62d5f7749f1a9a3d96b77e0964f7f49a0
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-06-07 12:11:23 +0300

    Problem install

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {I18nProvider} from '@lingui/react';
+import {Trans} from '@lingui/macro';
+import {i18n} from '@lingui/core';
+import {en} from 'make-plural/plurals';
+
+i18n.loadLocaleData('en', {plurals: en});
+i18n.load('en', messages);
+i18n.activate('en');
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
+    <I18nProvider i18n={i18n}>
+      <View style={styles.container}>
+        <Text>
+          <Trans>Message Inbox</Trans>
+        </Text>
+      </View>
+    </I18nProvider>
   );
 };
 
